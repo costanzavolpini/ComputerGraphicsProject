6a46a90607d084accd490ccbc973bba92932c601
Marco <marco.mt19@gmail.com>
2017-11-29 16:15:03 +0100

    String casting

--- salsa/CCanvas.h
+++ salsa/CCanvas.h
@@ -27,9 +27,9 @@ class CCanvas : public QGLWidget {
 
   public:
     explicit CCanvas(QWidget *parent = 0) : QGLWidget(parent),
-                                            textureTrain(BASE_PATH + "train.jpg"),
-                                            modelTrain(BASE_PATH + "train.obj"),
-                                            modelTrain2(BASE_PATH + "train.ply") {
+                                            textureTrain((string)BASE_PATH + (string)"train.jpg"),
+                                            modelTrain((string)BASE_PATH + (string)"train.obj"),
+                                            modelTrain2((string)BASE_PATH + (string)"train.ply") {
         QTimer *timer = new QTimer(this);
         connect(timer, SIGNAL(timeout()), this, SLOT(updateGL()));
         timer->start(10);
