4f96d72b9712f26ea6c9181b6c9e1bdb0c17e195
Costanza Volpini <costanza.volpini@usi.ch>
2017-12-15 18:58:46 +0100

    Added GLM
    TODO: solve problem with glm::mat4

--- salsa/CCanvas.cpp
+++ salsa/CCanvas.cpp
@@ -288,6 +288,22 @@ void CCanvas::paintGL() {
     bird.inc();
     bird.draw();
 
+    /**
+     * Shadow Management
+     */
+    glm::vec3 lightInvDir = glm::vec3(lightpos[0],lightpos[1],lightpos[2]);
+
+// Compute the MVP matrix from the light's point of view
+// glm::mat4 depthProjectionMatrix = glm::ortho<float>(-10,10,-10,10,-10,20);
+// glm::mat4 depthViewMatrix = glm::lookAt(lightInvDir, glm::vec3(0,0,0), glm::vec3(0,1,0));
+// glm::mat4 depthModelMatrix = glm::mat4(1.0);
+// glm::mat4 depthMVP = depthProjectionMatrix * depthViewMatrix * depthModelMatrix;
+
+ // Send our transformation to the currently bound shader,
+ // in the "MVP" uniform
+// glUniformMatrix4fv(depthMatrixID, 1, GL_FALSE, &depthMVP[0][0]);
+
+
 
     /*
      * Unbind texture and pop matrix
--- salsa/CCanvas.h
+++ salsa/CCanvas.h
@@ -17,6 +17,11 @@ using namespace std;
 #include "Bird.h"
 #include "Scene.h"
 #include "Sky.h"
+#include <glm/vec3.hpp> // glm::vec3
+#include <glm/vec4.hpp> // glm::vec4
+#include <glm/mat4x4.hpp> // glm::mat4
+#include <glm/gtc/matrix_transform.hpp> // glm::translate, glm::rotate, glm::scale, glm::perspective
+
 
 /************************************************************************/
 /* Canvas to draw                                                       */
